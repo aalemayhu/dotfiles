e623e5ebb141c64123ae0287b4c5168e9a50341a
Alexander Alemayhu <alexander.alemayhu@googlemail.com>
2013-07-12 15:05:59 +0200

    Add ssh-keygen.

--- new-machine/run-me.sh
+++ new-machine/run-me.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ./install-default-tools.sh && ./mk-default-directory.sh && ./place-config-files.sh
+sudo ./install-default-tools.sh && ./mk-default-directory.sh && ./place-config-files.sh && ssh-keygen -t rsa
