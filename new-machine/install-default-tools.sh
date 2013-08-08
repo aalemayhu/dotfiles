6dc10e610646f31aef1893593d5edb77eba897a0
Alexander Alemayhu <alexander.alemayhu@googlemail.com>
2013-08-08 22:32:15 +0200

    Add tig.

--- new-machine/install-default-tools.sh
+++ new-machine/install-default-tools.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-su -c "apt-get install sudo vim git-core suckless-tools rxvt-unicode xbindkeys x-window-system make autoconf build-essential libpng12-dev libxrender-dev libx11-dev libxinerama-dev libfreetype6-dev libxxf86vm-dev libglu1-mesa-dev libxft-dev libglew-dev links chromium-browser keepassx nicovideo-dl mplayer feh nitrogen scrot zathura xterm alpine console-data -y"
+su -c "apt-get install sudo vim git-core suckless-tools rxvt-unicode xbindkeys x-window-system make autoconf build-essential libpng12-dev libxrender-dev libx11-dev libxinerama-dev libfreetype6-dev libxxf86vm-dev libglu1-mesa-dev libxft-dev libglew-dev links chromium-browser keepassx nicovideo-dl mplayer feh nitrogen scrot zathura xterm alpine console-data tig -y"
