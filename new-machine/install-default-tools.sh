137cb8bda61479b192271a3b26b0a57e33f3db62
Alexander Alemayhu <alexander.alemayhu@googlemail.com>
2013-09-07 20:39:57 +0200

    Add arandr and atsign.

--- new-machine/install-default-tools.sh
+++ new-machine/install-default-tools.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt-get install sudo vim git-core suckless-tools rxvt-unicode xbindkeys x-window-system make autoconf build-essential libpng12-dev libxrender-dev libx11-dev libxinerama-dev libfreetype6-dev libxxf86vm-dev libglu1-mesa-dev libxft-dev libglew-dev links chromium-browser keepassx nicovideo-dl mplayer feh nitrogen scrot zathura xterm alpine console-data tig -y
+apt-get install sudo vim git-core suckless-tools rxvt-unicode xbindkeys x-window-system make autoconf build-essential libpng12-dev libxrender-dev libx11-dev libxinerama-dev libfreetype6-dev libxxf86vm-dev libglu1-mesa-dev libxft-dev libglew-dev links chromium-browser keepassx nicovideo-dl mplayer feh nitrogen scrot zathura xterm alpine console-data tig arandr -y
