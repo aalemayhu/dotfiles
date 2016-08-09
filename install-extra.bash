313be0f6f18d8b8dc42a3cf968fcadbaa106ab67
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-09 20:37:06 +0000

    Cleanup extra install script
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- install-extra.bash
+++ install-extra.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-gicp() {
-  GITHUB_USER=$(basename `pwd`)
-  GITHUB_PROJECT=$1
-  if [ ! -d "$GITHUB_PROJECT" ]; then
+clone_or_update() {
+  GITHUB_USER=$1
+  GITHUB_PROJECT=$2
+  EXPECTED_PATH=$GITHUB_ROOT/$GITHUB_USER/$GITHUB_PROJECT
+  if [ ! -d "$EXPECTED_PATH" ]; then
     echo Cloning $GITHUB_PROJECT
-    git clone https://github.com/$GITHUB_USER/$GITHUB_PROJECT
+    git clone https://github.com/$GITHUB_USER/$GITHUB_PROJECT $EXPECTED_PATH
   else
     echo There seems to already be directory named $GITHUB_PROJECT
     echo Updating instead.
-    cd $GITHUB_PROJECT
+    cd $EXPECTED_PATH
     git pull
     cd ..
   fi
@@ -17,99 +18,25 @@ gicp() {
 
 install_program() {
   echo Will be installing $1
-  cd $GITHUB_ME
+  cd $GITHUB_ROOT
   gicp $1
-  cd $GITHUB_ME/$1
+  cd $GITHUB_ROOT/$1
   autoreconf -i
   ./configure
   $S make install
 }
 
-install_go() {
-  cd $GITHUB_ME && gicp go
-  cd go/src/ && git ch go1.4
-  ./all.bash
-}
+main() {
+  GITHUB_ROOT=$HOME/src/github.com
+  clone_or_update mortehu cantera-wm
+  clone_or_update mortehu cantera-term
+  clone_or_update mortehu cantera-lock
+  clone_or_update git git
 
-create_directories() {
-  mkdir -p $GITHUB_ME
-  for d in `cat directories`; do
-    mkdir -p $HOME/$d
-  done
-}
-
-copy_config_files() {
-  cp $CONFIG_FILES_DIR/ssh/config $HOME/.ssh/config
-
-  cp $CONFIG_FILES_DIR/mutt/muttrc $HOME/.muttrc
-  cp -r $CONFIG_FILES_DIR/mutt/mutt/* $HOME/.mutt/
-  cp -r $CONFIG_FILES_DIR/mutt/mutt_files/* $HOME/.mutt_files/
-
-  cp $CONFIG_FILES_DIR/db/psqlrc $HOME/.psqlrc
-
-  cp $CONFIG_FILES_DIR/term/aliases $HOME/.term/.aliases
-  cp $CONFIG_FILES_DIR/term/funcs $HOME/.term/.funcs
-  cp $CONFIG_FILES_DIR/term/env $HOME/.term/.env
-
-  cp $CONFIG_FILES_DIR/git/gitconfig $HOME/.gitconfig
-  cp $CONFIG_FILES_DIR/git/git-completion.bash $HOME/.git-completion.bash
-
-  cp $CONFIG_FILES_DIR/term/bashrc $HOME/.bashrc
-  cp $CONFIG_FILES_DIR/term/lintianrc $HOME/.lintianrc
-
-  cp $CONFIG_FILES_DIR/vim/vimrc $HOME/.vimrc
-
-  cp $CONFIG_FILES_DIR/X/xbindkeysrc $HOME/.xbindkeysrc
-  cp $CONFIG_FILES_DIR/X/xsession $HOME/.xsession
-  cp $CONFIG_FILES_DIR/X/Xdefaults $HOME/.Xdefaults
-  cp $CONFIG_FILES_DIR/X/Xmodmap $HOME/.Xmodmap
-  cp $CONFIG_FILES_DIR/X/Xresources $HOME/.Xresources
-
-  cd $GITHUB_ME
-  gicp Vundle.vim
-  cd $GITHUB_ME
-  gicp liquidprompt
-
-  cp $CONFIG_FILES_DIR/gnupg/gpg.conf $HOME/.gnupg/
-}
-
-install_packages() {
-  $S apt-get update
-  $S apt-get install $(cat packages |tr '\n' ' ') -y
-}
-
-install_extra() {
-  install_packages
   install_program cantera-wm
   install_program cantera-term
   install_program cantera-lock
-  install_go
-}
-
-init() {
-  S=/usr/bin/sudo
-  GITHUB_ME=$HOME/src/github.com/scanf
-  CONFIG_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-  LIBS=$HOME/libs
-  if [ ! -d "$GITHUB_ME/config-files" ]; then
-    cd $GITHUB_ME
-    git clone https://github.com/scanf/config-files
-    cd config-files
-  fi
-}
-
-extras() {
-  if [[ $1 == *setup* ]]; then
-    install_extra
-    echo ssh-keygen -t rsa -C alexander@alemayhu.com
-  fi
-}
-
-main() {
-  init
-  create_directories
-  copy_config_files
-  extras $1
+  install_program git git
 }
 
 main $1
