c609bf138c45118c1718980bdcde133643337ba9
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-15 21:53:14 +0200

    Move directory creation into a own script
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

---
+++ create_directories.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+home=ENV['HOME']
+`mkdir -p #{home}/src/github.com/scanf`
+
+file='DirectoriesList'
+File.readlines(file).each do |directory|
+  `mkdir #{home}/#{directory}`
+end
+
--- run.bash
+++ run.bash
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-create_directories() {
-  mkdir -p $GITHUB_ME
-  for d in `cat $CONFIG_FILES_DIR/DirectoriesList`; do
-    mkdir -p $HOME/$d
-  done
-}
-
 configure() {
   S=/usr/bin/sudo
   GITHUB_ME=$HOME/src/github.com/scanf
@@ -25,7 +18,7 @@ install_X_desktop_essentials() {
 main() {
   configure
   $S $CONFIG_FILES_DIR/install_packages.rb
-  create_directories
+  $CONFIG_FILES_DIR/create_directories.rb
   $CONFIG_FILES_DIR/clone_or_update.rb
   $CONFIG_FILES_DIR/copy_files.rb
 
