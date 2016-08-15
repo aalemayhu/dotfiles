2d7f9d3c0e511022b38e990a011cc386f4c41ef5
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-15 21:18:37 +0200

    Sort repositories
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- clone_or_update.rb
+++ clone_or_update.rb
@@ -3,13 +3,13 @@
 home = "#{ENV['HOME']}"
 github = "#{home}/src/github.com"
 repositories = {
-    "#{github}/scanf/config-files" => "https://github.com/scanf/config-files",
     "#{github}/VundleVim/Vundle.vim.gi" => "https://github.com/VundleVim/Vundle.vim.git",
-    "#{github}/scanf/xcd.rb" => "https://github.com/scanf/xcd.rb",
-    "#{github}/mortehu/cantera-wm" => "https://github.com/mortehu/cantera-wm",
-    "#{github}/mortehu/cantera-term" => "https://github.com/mortehu/cantera-term",
-    "#{github}/mortehu/cantera-lock" => "https://github.com/mortehu/cantera-lock",
     "#{github}/git/git" => "https://github.com/git/git",
+    "#{github}/mortehu/cantera-lock" => "https://github.com/mortehu/cantera-lock",
+    "#{github}/mortehu/cantera-term" => "https://github.com/mortehu/cantera-term",
+    "#{github}/mortehu/cantera-wm" => "https://github.com/mortehu/cantera-wm",
+    "#{github}/scanf/config-files" => "https://github.com/scanf/config-files",
+    "#{github}/scanf/xcd.rb" => "https://github.com/scanf/xcd.rb",
     "#{home}/Scripts" => "https://github.com/scanf/Scripts",
 }
 
