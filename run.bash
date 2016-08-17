0b6e8628aeae69ae852d0f134e7d97faf7122234
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-17 14:02:08 +0200

    Move sudo calls out of main script
    This makes the run script functional on macOS.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- run.bash
+++ run.bash
@@ -3,9 +3,8 @@
 VERSION=0
 CONFIG_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 GITHUB_ME=$HOME/src/github.com/scanf
-S=/usr/bin/sudo
 
-$S $CONFIG_FILES_DIR/install_packages.rb
+$CONFIG_FILES_DIR/install_packages.rb
 $CONFIG_FILES_DIR/create_directories.rb
 $CONFIG_FILES_DIR/clone_or_update.rb
 $CONFIG_FILES_DIR/copy_files.rb
@@ -13,8 +12,6 @@ $CONFIG_FILES_DIR/copy_files.rb
 ln -sf $HOME/.vim/vimrc $HOME/.vimrc
 vim +PluginInstall +qa!
 
-cd $GITHUB_ME/xcd.rb
-$S make install
 
 if [ ! -d "$HOME/.ssh" ]; then
   ssh-keygen -t rsa -C alexander@alemayhu.com
