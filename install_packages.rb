aee38313e34a8495d6423017a888cefbbcefae1e
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-19 15:08:58 +0200

    We don't really care about the output so use system instead of backticks
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- install_packages.rb
+++ install_packages.rb
@@ -2,14 +2,14 @@
 
 unamestr = `uname`.strip
 if unamestr.start_with?('Linux')
-  `apt-get update`
-  `apt-get install git vim`
+  system('apt-get update')
+  system('apt-get install git vim')
   file='PackagesList'
   File.readlines(file).each do |package|
-    "apt-get instal -y #{package}"
+    system("apt-get instal -y #{package}")
   end
-  `apt-get autoremove`
-  `apt-get autoclean`
-  `locale-gen "en_US.UTF-8"`
-  `cd "#{ENV['Home']}"/src/github.com/scanf/xcd.rb && /usr/bin/sudo make install`
+  system('apt-get autoremove')
+  system('apt-get autoclean')
+  system('locale-gen "en_US.UTF-8"')
+  system('cd "#{ENV['Home']}"/src/github.com/scanf/xcd.rb && /usr/bin/sudo make install')
 end
