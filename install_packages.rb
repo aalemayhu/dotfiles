407a14fc882077039a835055e51d9ba65ebfa5f9
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-20 09:19:34 +0200

    Adding sudo for installing packages
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- install_packages.rb
+++ install_packages.rb
@@ -2,14 +2,14 @@
 
 unamestr = `uname`.strip
 if unamestr.start_with?('Linux')
-  system('apt-get update')
-  system('apt-get install git vim')
+  system('sudo apt-get update')
+  system('sudo apt-get install git vim')
   file='PackagesList'
   File.readlines(file).each do |package|
-    system("apt-get instal -y #{package}")
+    system("sudo apt-get install -y #{package}")
   end
-  system('apt-get autoremove')
-  system('apt-get autoclean')
+  system('sudo apt-get autoremove')
+  system('sudo apt-get autoclean')
   system('locale-gen "en_US.UTF-8"')
   system("cd #{ENV['Home']}/src/github.com/scanf/xcd.rb && /usr/bin/sudo make install")
 end
