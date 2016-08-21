02a060997d9ba9fefdb99a741fd7bd7dae1f8892
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-21 13:55:52 +0200

    Add support for Fedora package manager
    Most of the packages seem to be similiar, only exception detected was
    silversearcher-ag.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- install_packages.rb
+++ install_packages.rb
@@ -2,14 +2,23 @@
 
 unamestr = `uname`.strip
 if unamestr.start_with?('Linux')
-  system('sudo apt-get update')
-  system('sudo apt-get install git vim')
+  distro_id = `lsb_release -i`.strip
+  pm = "apt-get" # assuming Debian
+  if distro_id.end_with?("Fedora")
+   pm = "dnf"
+  end
+
+  system("sudo #{pm} update")
+  system("sudo #{pm} install git vim")
   file='PackagesList'
   File.readlines(file).each do |package|
-    system("sudo apt-get install -y #{package}")
+    system("sudo #{pm} install -y #{package}")
   end
-  system('sudo apt-get autoremove')
-  system('sudo apt-get autoclean')
+  system("sudo #{pm} autoremove")
+
+  if pm == "apt-get"
+   system("sudo #{pm} autoclean")
+  end
   system('locale-gen "en_US.UTF-8"')
   system("cd #{ENV['Home']}/src/github.com/scanf/xcd.rb && /usr/bin/sudo make install")
 end
