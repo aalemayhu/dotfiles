9b408908779fd0fc3acc5f661590fa2e6c4b1111
Alexander Alemayhu <alexander@alemayhu.com>
2016-09-17 21:32:14 +0200

    move scripts into a own directory
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,6 @@ WORKDIR /tmp/config-files
 
 RUN echo "postfix postfix/mailname string container.alemayhu.com" | debconf-set-selections
 RUN echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
-RUN /bin/bash run.bash
+RUN /bin/bash bin/run.bash
 RUN apt-get clean
 RUN rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
--- bin/run.bash
+++ bin/run.bash
@@ -4,10 +4,10 @@ VERSION=0
 CONFIG_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 GITHUB_ME=$HOME/src/github.com/scanf
 
-/usr/bin/sudo $CONFIG_FILES_DIR/install_packages.rb
-$CONFIG_FILES_DIR/create_directories.rb
-$CONFIG_FILES_DIR/clone_or_update.rb
-$CONFIG_FILES_DIR/copy_files.rb
+/usr/bin/sudo $CONFIG_FILES_DIR/bin/install_packages.rb
+$CONFIG_FILES_DIR/bin/create_directories.rb
+$CONFIG_FILES_DIR/bin/clone_or_update.rb
+$CONFIG_FILES_DIR/bin/copy_files.rb
 
 ln -sf $HOME/.vim/.vimrc $HOME/.vimrc
 ln -sf $HOME/.mutt/.muttrc $HOME/.muttrc
