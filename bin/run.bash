9e7803aa6882f99df0acf8461e7a3dc36f84e9dd
Alexander Alemayhu <alexander@alemayhu.com>
2016-09-17 21:44:39 +0200

    bin/run: correct paths to bin directory and some config
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- bin/run.bash
+++ bin/run.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 VERSION=0
-CONFIG_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+BIN_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 GITHUB_ME=$HOME/src/github.com/scanf
 
-/usr/bin/sudo $CONFIG_FILES_DIR/bin/install_packages.rb
-$CONFIG_FILES_DIR/bin/create_directories.rb
-$CONFIG_FILES_DIR/bin/clone_or_update.rb
-$CONFIG_FILES_DIR/bin/copy_files.rb
+/usr/bin/sudo $BIN_DIRECTORY/install_packages.rb
+$BIN_DIRECTORY/create_directories.rb
+$BIN_DIRECTORY/clone_or_update.rb
+$BIN_DIRECTORY/copy_files.rb
 
 ln -sf $HOME/.vim/.vimrc $HOME/.vimrc
 ln -sf $HOME/.mutt/.muttrc $HOME/.muttrc
 vim +PluginInstall +qa!
 
 if [ ! -f $HOME/.msmtprc ]; then
-  cp $CONFIG_FILES_DIR/msmtprc $HOME/.msmtprc
+  cp $BIN_DIRECTORY/msmtprc $HOME/.msmtprc
 fi
 if [ ! -f $HOME/.offlineimaprc ]; then
-  cp $CONFIG_FILES_DIR/mutt/offlineimaprc $HOME/.offlineimaprc
+  cp $BIN_DIRECTORY/../mutt/offlineimaprc $HOME/.offlineimaprc
 fi
 
 if [ ! -d "$HOME/.ssh" ]; then
