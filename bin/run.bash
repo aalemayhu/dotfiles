cc9814668c5b4da63101c084b482b55f157a16bf
Alexander Alemayhu <alexander@alemayhu.com>
2016-09-18 12:39:48 +0000

    bin/run: remove sudo, let install_packages handle it
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- bin/run.bash
+++ bin/run.bash
@@ -4,7 +4,7 @@ VERSION=0
 BIN_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 GITHUB_ME=$HOME/src/github.com/scanf
 
-/usr/bin/sudo $BIN_DIRECTORY/install_packages.rb
+$BIN_DIRECTORY/install_packages.rb
 $BIN_DIRECTORY/create_directories.rb
 $BIN_DIRECTORY/clone_or_update.rb
 $BIN_DIRECTORY/copy_files.rb
