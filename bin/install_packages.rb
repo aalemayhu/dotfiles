7b6fed727dcb1c21239950a84ec495228ae843fe
Alexander Alemayhu <alexander@alemayhu.com>
2016-09-18 12:46:43 +0000

    bin/install_packages: refactor most sudo calls into own method
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- bin/install_packages.rb
+++ bin/install_packages.rb
@@ -1,21 +1,27 @@
 #!/usr/bin/ruby
 
 unamestr = `uname`.strip
+sudo_path = "/usr/bin/sudo"
+
+def run_as_priviliged_user(cmd)
+  system("#{sudo_path} #{cmd}"
+end
+
 if unamestr.start_with?('Linux')
   pm = "apt-get" # assuming Debian
   if File.exist?("/etc/fedora-release")
    pm = "dnf"
   end
 
-  system("sudo #{pm} update")
-  system("sudo #{pm} install git vim")
+  run_as_priviliged_user("#{pm} update")
+  run_as_priviliged_user("#{pm} install git vim")
   packages = File.readlines("PackagesList").join(" ").delete!("\n")
-  system("sudo #{pm} install -y #{packages}")
-  system("sudo #{pm} autoremove")
+  run_as_priviliged_user("#{pm} install -y #{packages}")
+  run_as_priviliged_user("#{pm} autoremove")
 
   if pm == "apt-get"
-   system("sudo #{pm} autoclean")
+   run_as_priviliged_user("#{pm} autoclean")
   end
-  system('sudo locale-gen "en_US.UTF-8"')
-  system("cd #{ENV['HOME']}/src/github.com/scanf/xcd.rb && /usr/bin/sudo make install")
+  run_as_priviliged_user("locale-gen \"en_US.UTF-8\"")
+  system("cd #{ENV['HOME']}/src/github.com/scanf/xcd.rb && #{sudo_path} make install")
 end
