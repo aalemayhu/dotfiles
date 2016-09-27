26e4df9afb1d2f8b34e23c63017b0431509af58d
Alexander Alemayhu <alexander@alemayhu.com>
2016-09-27 18:51:29 +0200

    bin/install_packages: add support for macOO
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- bin/install_packages.rb
+++ bin/install_packages.rb
@@ -13,23 +13,28 @@ if !File.exist?(sudo_path)
   exit
 end
 
-if `uname`.strip.start_with?('Linux')
-  pm = "apt-get" # assuming Debian
-  packages = "Debian.packages"
-  if File.exist?("/etc/fedora-release")
-   pm = "dnf"
-   packages = "Fedora.packages"
-  end
+pm = "apt-get" # assuming Debian
+packages = "Debian.packages"
+if File.exist?("/etc/fedora-release")
+  pm = "dnf"
+  packages = "Fedora.packages"
+end
 
+if `uname`.strip.start_with?('Darwin')
+  pm = "brew"
+  packages = "macOS.packages"
+  packages = File.readlines(packages).join(" ").delete!("\n")
+  `#{pm} install #{packages}`
+else
+  packages = File.readlines(packages).join(" ").delete!("\n")
   run_as_priviliged_user("#{pm} update")
   run_as_priviliged_user("#{pm} install git vim")
-  packages = File.readlines(packages).join(" ").delete!("\n")
   run_as_priviliged_user("#{pm} install -y #{packages}")
   run_as_priviliged_user("#{pm} autoremove")
 
   if pm == "apt-get"
-   run_as_priviliged_user("#{pm} autoclean")
+    run_as_priviliged_user("#{pm} autoclean")
   end
   run_as_priviliged_user("locale-gen \"en_US.UTF-8\"")
-  system("cd #{ENV['HOME']}/src/github.com/scanf/xcd.rb && #{sudo_path} make install")
 end
+system("cd #{ENV['HOME']}/src/github.com/scanf/xcd.rb && #{sudo_path} make install")
