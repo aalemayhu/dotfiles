8bde8bcf915c2563beb1f8ee317692e99eee4509
Alexander Alemayhu <alexander@alemayhu.com>
2016-12-20 20:56:46 +0100

    Revert "Delete X related stuff for now"
    This reverts commit d1bd5721addcf9291248cd0d1a6ab048f8f2874c.

---
+++ bin/setup-desktop.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CONFIG_FILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+install_program() {
+  GITHUB_USER=$1
+  GITHUB_PROJECT=$2
+  EXPECTED_PATH=$GITHUB_ROOT/$GITHUB_USER/$GITHUB_PROJECT
+  echo Will be installing $GITHUB_PROJECT
+  cd $EXPECTED_PATH
+  autoreconf -i
+  ./configure
+  $S make -j install
+  cd -
+}
+
+main() {
+  S=/usr/bin/sudo
+  GITHUB_ROOT=$HOME/src/github.com
+
+  install_program mortehu cantera-wm
+  install_program mortehu cantera-term
+  install_program mortehu cantera-lock
+
+  $S modprobe -rvf psmouse
+}
+
+
+bin/clone_or_update.rb desktop
+main $1
+
+# X11
+
+cp $CONFIG_FILES_DIR/X/xbindkeysrc $HOME/.xbindkeysrc
+cp $CONFIG_FILES_DIR/X/xsession $HOME/.xsession
+cp $CONFIG_FILES_DIR/X/Xdefaults $HOME/.Xdefaults
+cp $CONFIG_FILES_DIR/X/Xmodmap $HOME/.Xmodmap
+cp $CONFIG_FILES_DIR/X/Xresources $HOME/.Xresources
