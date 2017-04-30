7a9b7afb500d6f4a60f99c2cc051297a72e16323
Alexander Alemayhu <alexander@alemayhu.com>
2017-04-30 12:44:02 +0200

    bin/clone_or_update: perform git operations in background
    This should speed up initial run a little bit.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- bin/clone_or_update.rb
+++ bin/clone_or_update.rb
@@ -6,12 +6,12 @@ github = "#{home}/src/github.com"
 def clone_or_update(repo)
   repo.each_pair do |path, url|
     if !File.exist?(path)
-      `git clone #{url} #{path} --recurse-submodules`
+      `git clone #{url} #{path} --recurse-submodules &`
     elsif !File.exist?("#{path}/.git")
       STDERR.puts "error: directory exists at #{path} but is not a git directory"
       exit
     else
-      `git -C #{path} pull --recurse-submodules`
+      `git -C #{path} pull --recurse-submodules &`
     end
   end
 end
