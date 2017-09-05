55a619ac1037e568dd8924a9391cb542fad40d28
Alexander Alemayhu <alexander@alemayhu.com>
2017-09-05 10:29:40 +0200

    bin/clone_or_update: add cilium to list of repositories
    Makes it easier to get started on a test vm for work.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- bin/clone_or_update.rb
+++ bin/clone_or_update.rb
@@ -26,6 +26,7 @@ clone_or_update({
   "#{home}/.mutt" => "https://github.com/scanf/.mutt",
   "#{github}/ggreer/the_silver_searcher" => "https://github.com/ggreer/the_silver_searcher",
   "#{github}/vrothberg/vgrep" => "https://github.com/vrothberg/vgrep",
+  "#{home}/opt/og/src/github.com/cilium/cilium" => "https://github.com/cilium/cilium",
 })
 
 if not ARGV[0]
