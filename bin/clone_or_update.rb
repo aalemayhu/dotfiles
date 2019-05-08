d666f92e0b9b4ed7b522ce02a5a5762b8ae17a49
Alexander Alemayhu <alexander@alemayhu.com>
2019-05-08 17:38:46 +0200

    bin: remove outdated repositories
    Stuff I am not using on a daily basis. Want to keep git but have not
    been peeking into the source code for a while.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- bin/clone_or_update.rb
+++ bin/clone_or_update.rb
@@ -18,22 +18,13 @@ end
 
 clone_or_update({
   "#{github}/VundleVim/Vundle.vim" => "https://github.com/VundleVim/Vundle.vim.git",
-  "#{github}/git/git" => "https://github.com/git/git",
   "#{github}/scanf/dotfiles" => "https://github.com/scanf/dotfiles",
   "#{github}/scanf/xcd.rb" => "https://github.com/scanf/xcd.rb",
-  "#{home}/Scripts" => "https://github.com/scanf/Scripts",
   "#{home}/.vim" => "https://github.com/scanf/.vim",
   "#{home}/.mutt" => "https://github.com/scanf/.mutt",
   "#{github}/ggreer/the_silver_searcher" => "https://github.com/ggreer/the_silver_searcher",
-  "#{home}/opt/og/src/github.com/cilium/cilium" => "https://github.com/cilium/cilium",
 })
 
 if not ARGV[0]
   exit 0
 end
-
-clone_or_update({
-  "#{github}/scanf/cantera-lock" => "https://github.com/scanf/cantera-lock",
-  "#{github}/scanf/cantera-term" => "https://github.com/scanf/cantera-term",
-  "#{github}/scanf/cantera-wm" => "https://github.com/scanf/cantera-wm",
-})
