75cb718331845cfce0e98d35826ab38e22a283bf
Alexander Alemayhu <alexander@alemayhu.com>
2021-12-31 21:56:06 +0100

    bin/run: rewrite in Ruby

---
+++ bin/run.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby -w
+# frozen_string_literal: true
+
+require_relative './create_directories'
+require_relative './copy_files.ts'
+require_relative './clone_or_update.ts'
+require_relative './install_packages.ts'
+
+install_packages
+create_directories(home, "#{File.dirname(__FILE__)}/../DirectoriesList")
+synchronize_repositories
+copy_files
+
+vimrc = "#{home}/.vimrc"
+File.symlink("#{home}/.vim/.vimrc", vimrc) unless File.exist?(vimrc)
+
+system('vim', '+PluginInstall', '+qa!')
+system('ssh-keygen', '-t', 'ed25519', '-C', 'a@alemayhu.com') unless File.exist?("#{home}/.ssh/id_ed25519")
